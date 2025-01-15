7a4b97666caf69f5ce5c279575410b60a817c715
salmanursholihah <salmanursholihah@gmail.com>
2025-01-15 10:20:08 +0700

    up praktik pertama

---
+++ resources/views/praktik/struktur_html.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>struktur html</title>
+</head>
+<body>
+    <h1>praktik struktur dasar html</h1>
+    <p>ini merupakan contoh struktur dasar html</p>
+    <a href="https://milospro.com/blog-details/command-yang-sering-digunakan-dalam-laravel">kunjungi website</a>
+</body>
+</html>
