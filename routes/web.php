e72afafdc6a1df03da6e7f66549d8e868a61009b
salmanursholihah <salmanursholihah@gmail.com>
2025-01-15 10:23:09 +0700

    struktur_dasar_html

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/struktur_dasar_html', function () {
+    return view('praktik.struktur_html');
+});
